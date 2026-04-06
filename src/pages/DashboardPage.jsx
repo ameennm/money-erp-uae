380fc19397b37c61ccc18668b4ca4239ada16ef8
ameennm <muhammedmusthafaameennm@gmail.com>
2026-04-06 18:28:00 +0530

    Restore missing icons in DashboardPage.jsx

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -4,7 +4,7 @@ import { ledgerService } from '../lib/ledgerService';
 import Layout from '../components/Layout';
 import {
     TrendingUp, SendHorizonal,
-    X, PiggyBank
+    Banknote, Wallet, X, PiggyBank
 } from 'lucide-react';
 import { useAuth } from '../context/AuthContext';
 import toast from 'react-hot-toast';
